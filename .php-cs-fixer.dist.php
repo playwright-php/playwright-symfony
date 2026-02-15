753f16e31ec55f91386d48487c123d8b0bb68f10
Simon André <smn.andre@gmail.com>
2026-02-15 22:30:07 +0100

    Update php-cs-fixer conf

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,14 +4,18 @@ $license = <<<LICENSE
 This file is part of the community-maintained Playwright PHP project.
 It is not affiliated with or endorsed by Microsoft.
 
-(c) 2025-Present - Playwright PHP <https://github.com/playwright-php>
+(c) 2025-Present - Playwright PHP - https://github.com/playwright-php
 
 For the full copyright and license information, please view the LICENSE
 file that was distributed with this source code.
 LICENSE;
 
 $finder = (new PhpCsFixer\Finder())
-    ->in(__DIR__);
+    ->in([
+        __DIR__.'/config',
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ]);
 
 return (new PhpCsFixer\Config())
     ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
@@ -21,5 +25,6 @@ return (new PhpCsFixer\Config())
         '@Symfony' => true,
         'declare_strict_types' => true,
         'header_comment' => ['header' => $license],
+        'no_unused_imports' => true,
     ])
 ;
