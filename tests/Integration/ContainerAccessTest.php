a87a496c28a1e43d4618089c453685318d4f1a55
Kevin Bond <kevinbond@gmail.com>
2026-08-08 12:40:01 -0400

    Add `getContainer()` to the kernel client

---
+++ tests/Integration/ContainerAccessTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the community-maintained Playwright PHP project.
+ * It is not affiliated with or endorsed by Microsoft.
+ *
+ * (c) 2025-Present - Playwright PHP - https://github.com/playwright-php
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Playwright\Symfony\Tests\Integration;
+
+use PHPUnit\Framework\Attributes\CoversNothing;
+use Playwright\Symfony\Test\PlaywrightTestCase;
+use Playwright\Symfony\Tests\Fixtures\App\TestKernel;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpKernel\KernelInterface;
+
+#[CoversNothing]
+final class ContainerAccessTest extends PlaywrightTestCase
+{
+    protected static function createKernel(array $options = []): KernelInterface
+    {
+        return new TestKernel('test', false);
+    }
+
+    public function testGetContainerReturnsTheContainer(): void
+    {
+        $container = $this->client->getContainer();
+
+        self::assertInstanceOf(ContainerInterface::class, $container);
+        self::assertTrue($container->has('kernel'));
+    }
+
+    public function testGetContainerPrefersTheTestContainer(): void
+    {
+        $container = $this->client->getContainer();
+        $realContainer = self::$kernel?->getContainer();
+
+        self::assertNotNull($realContainer);
+        self::assertNotSame($realContainer, $container, 'expected the test container, which exposes private services');
+        self::assertSame($realContainer->get('test.service_container'), $container);
+    }
+}
